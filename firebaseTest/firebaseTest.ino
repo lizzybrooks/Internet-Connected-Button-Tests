5210ef56d808a6544bb097491141f88a3f7fc382
Raphael Gonzalez <raphaelgonzalez1837@gmail.com>
2019-05-22 09:24:38 -0700

    Update firebaseTest.ino

--- firebaseTest/firebaseTest.ino
+++ firebaseTest/firebaseTest.ino
@@ -1,5 +1,4 @@
 
-
 /*
  * Created by K. Suwatchai (Mobizt)
  * 
@@ -24,7 +23,8 @@
 //Define FirebaseESP8266 data object
 FirebaseData firebaseData;
 
-void setup(){
+void setup()
+{
 
   Serial.begin(115200);
 
@@ -56,56 +56,235 @@ void setup(){
   Firebase.enableClassicRequest(firebaseData, true);
   */
 
-  String path = "/class";
+  // String path = "/ESP8266_Test";
+  String path = "/class" // new path to jeffrey's database architcture 
 
   String jsonStr = "";
  
   Serial.println("------------------------------------");
   Serial.println("Set double test...");
 
-  for (int i = 0; i < 10; i++)
+// __________________
+//   if (Firebase.setDouble(firebaseData, path + "/Double/Data" + 10) ))
+//   {
+//       Serial.println("PASSED");
+//       Serial.println("PATH: " + firebaseData.dataPath());
+//       Serial.println("TYPE: " + firebaseData.dataType());
+//       Serial.println("ETag: " + firebaseData.ETag());
+//       Serial.print("VALUE: ");
+//       if (firebaseData.dataType() == "int")
+//         Serial.println(firebaseData.intData());
+//       else if (firebaseData.dataType() == "float")
+//         Serial.println(firebaseData.floatData(), 5);
+//       else if (firebaseData.dataType() == "double")
+//         Serial.println(firebaseData.doubleData(), 9);
+//       else if (firebaseData.dataType() == "boolean")
+//         Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+//       else if (firebaseData.dataType() == "string")
+//         Serial.println(firebaseData.stringData());
+//       else if (firebaseData.dataType() == "json")
+//         Serial.println(firebaseData.jsonData());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//   }
+//   else
+//   {
+//       Serial.println("FAILED");
+//       Serial.println("REASON: " + firebaseData.errorReason());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//   }
+//  //________________________
+
+//   Serial.println("------------------------------------");
+//   Serial.println("Get double test...");
+
+//   for (uint8_t i = 0; i < 10; i++)
+//   {
+
+//     if (Firebase.getInt(firebaseData, path + "/Double/Data" + (i + 1)))
+//     {
+//       Serial.println("PASSED");
+//       Serial.println("PATH: " + firebaseData.dataPath());
+//       Serial.println("TYPE: " + firebaseData.dataType());
+//       Serial.println("ETag: " + firebaseData.ETag());
+//       Serial.print("VALUE: ");
+//       if (firebaseData.dataType() == "int")
+//         Serial.println(firebaseData.intData());
+//       else if (firebaseData.dataType() == "float")
+//         Serial.println(firebaseData.floatData(), 5);
+//       else if (firebaseData.dataType() == "double")
+//         Serial.println(firebaseData.doubleData(), 9);
+//       else if (firebaseData.dataType() == "boolean")
+//         Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+//       else if (firebaseData.dataType() == "string")
+//         Serial.println(firebaseData.stringData());
+//       else if (firebaseData.dataType() == "json")
+//         Serial.println(firebaseData.jsonData());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//     else
+//     {
+//       Serial.println("FAILED");
+//       Serial.println("REASON: " + firebaseData.errorReason());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//   }
+
+//   Serial.println("------------------------------------");
+//   Serial.println("Push integer test...");
+
+//   for (uint8_t i = 0; i < 5; i++)
+//   {
+
+//     if (Firebase.pushInt(firebaseData, path + "/Push/Int", (i + 1)))
+//     {
+//       Serial.println("PASSED");
+//       Serial.println("PATH: " + firebaseData.dataPath());
+//       Serial.print("PUSH NAME: ");
+//       Serial.println(firebaseData.pushName());
+//       Serial.println("ETag: " + firebaseData.ETag());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//     else
+//     {
+//       Serial.println("FAILED");
+//       Serial.println("REASON: " + firebaseData.errorReason());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//   }
+
+//   Serial.println("------------------------------------");
+//   Serial.println("Push JSON test...");
+
+//   for (uint8_t i = 5; i < 10; i++)
+//   {
+
+//     jsonStr = "{\"Data" + String(i + 1) + "\":" + String(i + 1) + "}";
+
+//     if (Firebase.pushJSON(firebaseData, path + "/Push/Int", jsonStr))
+//     {
+//       Serial.println("PASSED");
+//       Serial.println("PATH: " + firebaseData.dataPath());
+//       Serial.print("PUSH NAME: ");
+//       Serial.println(firebaseData.pushName());
+//       Serial.println("ETag: " + firebaseData.ETag());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//     else
+//     {
+//       Serial.println("FAILED");
+//       Serial.println("REASON: " + firebaseData.errorReason());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//   }
+
+//   Serial.println("------------------------------------");
+//   Serial.println("Update test...");
+
+//   for (uint8_t i = 0; i < 5; i++)
+//   {
+
+//     jsonStr = "{\"Data" + String(i + 1) + "\":" + String(i + 5.5) + "}";
+
+//     if (Firebase.updateNode(firebaseData, path + "/Int", jsonStr))
+//     {
+//       Serial.println("PASSED");
+//       Serial.println("PATH: " + firebaseData.dataPath());
+//       Serial.println("TYPE: " + firebaseData.dataType());
+//       //No ETag available
+//       Serial.print("VALUE: ");
+//       if (firebaseData.dataType() == "int")
+//         Serial.println(firebaseData.intData());
+//       else if (firebaseData.dataType() == "float")
+//         Serial.println(firebaseData.floatData(), 5);
+//       else if (firebaseData.dataType() == "double")
+//         Serial.println(firebaseData.doubleData(), 9);
+//       else if (firebaseData.dataType() == "boolean")
+//         Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
+//       else if (firebaseData.dataType() == "string")
+//         Serial.println(firebaseData.stringData());
+//       else if (firebaseData.dataType() == "json")
+//         Serial.println(firebaseData.jsonData());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//     else
+//     {
+//       Serial.println("FAILED");
+//       Serial.println("REASON: " + firebaseData.errorReason());
+//       Serial.println("------------------------------------");
+//       Serial.println();
+//     }
+//   }
+  push_data(1, "test_yr")
+}
+
+Int get_data(Int pn_nm, String cls_yr)
+{
+  if (Firebase.getInt(firebaseData, "class/"+cls_yr))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.println("TYPE: " + firebaseData.dataType());
+      Serial.println("ETag: " + firebaseData.ETag());
+      Serial.print("VALUE: ")
+      Serial.println(firebaseData.intData());
+      return firebaseData.intData()
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+      return -1
+    }
+}
+
+void push_data( Int pin_name, String class_yr )
+{
+  Int cur_val = get_data(pin_name, class_yr)
+  if (cur_val != -1)
   {
+    jsonStr = "{" + class_yr + "\":" + String(cur_val+1) + "}";
 
-//    if (
-      Firebase.setDouble(firebaseData, path + "/srtickets/bike" + (i + 1), ((i + 1) * 10) + 0.123456789);
-//      )
-//    {
-//      Serial.println("PASSED");
-//      Serial.println("PATH: " + firebaseData.dataPath());
-//      Serial.println("TYPE: " + firebaseData.dataType());
-//      Serial.println("ETag: " + firebaseData.ETag());
-//      Serial.print("VALUE: ");
-//      if (firebaseData.dataType() == "int")
-//        Serial.println(firebaseData.intData());
-//      else if (firebaseData.dataType() == "float")
-//        Serial.println(firebaseData.floatData(), 5);
-//      else if (firebaseData.dataType() == "double")
-//        Serial.println(firebaseData.doubleData(), 9);
-//      else if (firebaseData.dataType() == "boolean")
-//        Serial.println(firebaseData.boolData() == 1 ? "true" : "false");
-//      else if (firebaseData.dataType() == "string")
-//        Serial.println(firebaseData.stringData());
-//      else if (firebaseData.dataType() == "json")
-//        Serial.println(firebaseData.jsonData());
-//      Serial.println("------------------------------------");
-//      Serial.println();
-//    }
-//    else
-//    {
-//      Serial.println("FAILED");
-//      Serial.println("REASON: " + firebaseData.errorReason());
-//      Serial.println("------------------------------------");
-//      Serial.println();
-//    }
+    if (Firebase.pushJSON(firebaseData, "class", jsonStr))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.print("PUSH NAME: ");
+      Serial.println(firebaseData.pushName());
+      Serial.println("ETag: " + firebaseData.ETag());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
   }
-
-  Serial.println("------------------------------------");
-  Serial.println("Get double test...");
-
-
-
 }
 
 void loop()
 {
+  
+  
+  String path = "/class" // new path to jeffrey's database architcture 
+
+  String jsonStr = "";
+ 
+  Serial.println("------------------------------------");
+  
+  // pins 12 13 14 16
+  
 }
